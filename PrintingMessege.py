307c28331df0d9c72336f3e1ab85dffc8809aeae
Yajnesh Kumar <115884012+yajneshkumar316@users.noreply.github.com>
2023-08-21 22:09:05 +0530

    PrintingMessege.py

--- PrintingMessege.py
+++ PrintingMessege.py
@@ -1,1 +1,14 @@
+# first install module
+# in the terminal type
+# pip3 install pyautogui
 
+import pyautogui
+import time
+time.sleep(4)      # Will wait for 4 seconds before running the program
+count = 0
+
+# This will print Hello World 10 times wherever the cursor is placed
+while count<10:
+    pyautogui.typewrite("Hello World!")
+    pyautogui.press("Enter")    # This will print enter
+    count += 1
